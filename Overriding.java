e5088be8d7ed3177a3312ac8e47a2e77eefc5a29
abshar03 <48049572+abshar03@users.noreply.github.com>
2019-02-28 08:38:10 +0530

    Comment Added

--- Overriding.java
+++ Overriding.java
@@ -1,12 +1,15 @@
+/Java Program to illustrate the use of Java Method Overriding  
+//Creating a parent class. 
 class Vehicle{  
+//defining a method  
   void run(){System.out.println("Vehicle is running");}  
 }  
-
+//Creating a child class  
 class Bike extends Vehicle{  
   public static void main(String args[]){  
- 
+ //creating an instance of child class  
   Bike obj = new Bike();  
-  
+  //calling the method with child class instance  
   obj.run();  
   }  
 }  
